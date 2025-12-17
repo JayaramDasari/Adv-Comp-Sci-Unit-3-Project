c9d17786e39836e82bb741bfd9f34f63aad346ba
JayaramDasari <jayaram.dasari@thekaustschool.org>
2025-12-17 22:24:50 +0300

    Day 1

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-print("Hello world")
-print("123")
+import matplotlib.pyplot as plt
+
+def average(numbers):
+    return sum(numbers) / len(numbers)
+
+def highest_score(numbers):
+    return max(numbers)
+
+
+class ScoreTracker:
+    def __init__(self):
+        self.data = {}
+
+        def add_score(self, subject, score):
+            subject = subject.title()
+
+            if subject not in self.data:
+                self.data[subject] = []
+
+            self.data[subject].append(score)
+
+        def show_summary(self):
+            if not self.data:
+                print("No scores entered yet.")
+                return
+
+            print("\n--- Summary ---")
+            for item in self.data.items():
+                subject = item[0]
+                scores = item[1]
+
+                avg = average(scores)
+                high = highest_score(scores)
